70b0284dcdd5ff63d7a2901065dae5d5f557a3d6
Muthupandi <128305368+MuthuPandi-1963@users.noreply.github.com>
2024-01-20 19:41:39 +0530

    Create fruits_list.py

---
+++ Python_Practices/function_problem/fruits_list.py
@@ -0,0 +1,8 @@
+def myFruits(f1,f2,f3,f4):
+    FruitsList = [f1,f2,f3,f4]
+    return FruitsList
+    
+output = myFruits("Apple","Bannana","Grapes","Orange")
+print(output)
+
+#sfbshfbhsbfh shfbshffbjsbbf

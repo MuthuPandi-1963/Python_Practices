b764964437842d5b26527ddc98613538f4c7f674
Muthupandi <128305368+Prank879@users.noreply.github.com>
2024-01-20 10:49:51 +0530

    Create string_to_list.py
    convert a str to list

---
+++ Python_Practices/string_to_list.py
@@ -0,0 +1,9 @@
+# str="muthupandi" #String
+# list=list(str) # conversion code of str to list
+# print(list) #output statement
+
+# from the user input to conert a str to list
+
+str = input("enter a word : ")
+list=list(str)
+print(list)
